f0231cac8a6391f1ba3774daff18799947f6117e
Artur-Checha <artur.energetic@gmail.com>
2020-10-04 01:25:43 +0300

    feat: all conditions resolved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+	return ((array === undefined) || (array.length === 0)) ? 0 : Math.min.apply(0, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+	return ((array === undefined) || (array.length === 0)) ? 0 : Math.max.apply(0, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if ((array === undefined) || (array.length === 0))
+	return 0;
+	else {
+	let body = 0;
+	for (let i = 0; i < array.length; i++) {
+	body += array[i];
+	}
+	return body / array.length;
+	}
 }
